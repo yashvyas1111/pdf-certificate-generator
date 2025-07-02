cf962e3d84d1ff1ba219da86cbf2c327a9887c82
yashvyas <yvyas9646@gmail.com>
2025-07-02 13:28:21 +0530

    qty 2 not requires

--- server/controllers/certificateController.js
+++ server/controllers/certificateController.js
@@ -378,7 +378,7 @@ export const downloadCertificatePdf = async (req, res) => {
 
     res.set({
       'Content-Type': 'application/pdf',
-      'Content-Disposition': `attachment; filename="${certificateNo}.pdf"`,
+      'Content-Disposition': `inline; filename="${certificateNo}.pdf"`,
     });
     res.send(pdfBuffer);
   } catch (err) {
